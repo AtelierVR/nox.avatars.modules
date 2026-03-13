b191f719e4f5277f6fec8c6dc362ada926e6be8b
Hactazia <hactazia@gmail.com>
2026-03-13 01:38:06 +0100

    refactor: access modifiers for methods

--- Parameters/PlayableBaseParameter.cs
+++ Parameters/PlayableBaseParameter.cs
@@ -6,22 +6,22 @@ namespace Nox.CCK.Avatars.Parameters {
 		internal AnimatorControllerPlayable Controller;
 		internal Animator                   Animator;
 
-		protected override void SetFloat(float value)
+		override protected void SetFloat(float value)
 			=> Animator.SetFloat(GetKey(), value);
 
-		protected override void SetInteger(int value)
+		override protected void SetInteger(int value)
 			=> Animator.SetInteger(GetKey(), value);
 
-		protected override void SetBool(bool value)
+		override protected void SetBool(bool value)
 			=> Animator.SetBool(GetKey(), value);
 
-		protected override bool GetBool()
+		override protected bool GetBool()
 			=> Controller.GetBool(GetKey());
 
-		protected override float GetFloat()
+		override protected float GetFloat()
 			=> Controller.GetFloat(GetKey());
 
-		protected override int GetInteger()
+		override protected int GetInteger()
 			=> Controller.GetInteger(GetKey());
 	}
 }
